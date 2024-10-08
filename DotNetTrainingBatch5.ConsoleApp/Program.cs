ba03930a475539ef488700e23cd7f5654f7debc9
Wunna8834 <wunnaaungsayargyi99@gmail.com>
2024-10-08 23:04:32 +0630

    feat: add to db

--- DotNetTrainingBatch5.ConsoleApp/Program.cs
+++ DotNetTrainingBatch5.ConsoleApp/Program.cs
@@ -23,13 +23,43 @@ SqlCommand cmd = new SqlCommand(query, connection);
 // DataTable dt = new DataTable();
 // adapter.Fill(dt);
 SqlDataReader reader = cmd.ExecuteReader();
+
 while (reader.Read())
 {
     Console.WriteLine($"{reader["BlogId"]} {reader["BlogTitle"]} {reader["BlogAuthor"]} {reader["BlogContent"]}");
 }
-
-
-Console.WriteLine("Connection closing");
 connection.Close();
-Console.WriteLine("Connection closed");
+
+Console.WriteLine("Blog Title: ");
+string title = Console.ReadLine();
+
+Console.WriteLine("Blog Author: ");
+string author = Console.ReadLine();
+
+Console.WriteLine("Blog Content: ");
+string content = Console.ReadLine();
+
+connection.Open();
+string queryInsert = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+SqlCommand cmd2 = new SqlCommand(queryInsert, connection);
+cmd2.Parameters.AddWithValue("@BlogTitle", title);
+cmd2.Parameters.AddWithValue("@BlogAuthor", author);
+cmd2.Parameters.AddWithValue("@BlogContent", content);
+// SqlDataAdapter adapter = new SqlDataAdapter(cmd2);
+// DataTable dt = new DataTable();
+// adapter.Fill(dt);
+
+int result = cmd2.ExecuteNonQuery();
+connection.Close();
+Console.WriteLine(result == 1 ? "Saving Successful" : "Saving Failed");
 Console.ReadKey();
