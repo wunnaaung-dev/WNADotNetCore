42100177d52b5f9c5f38e6ac8c80e7aeef838919
Wunna8834 <wunnaaungsayargyi99@gmail.com>
2024-10-09 00:03:49 +0630

    feat: class file separated

---
+++ DotNetTrainingBatch5.ConsoleApp/AdoDotNetExample/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.SqlClient;
+
+namespace AdoDotNetExample;
+
+public class AdoDotNet
+{
+    private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+    public void Read()
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+        Console.WriteLine("Connection opening");
+        connection.Open();
+        Console.WriteLine("Connect opened");
+
+        string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog]";
+        SqlCommand cmd = new SqlCommand(query, connection);
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader["BlogId"]} {reader["BlogTitle"]} {reader["BlogAuthor"]} {reader["BlogContent"]}");
+        }
+        connection.Close();
+    }
+
+    public void Create()
+    {
+        Console.WriteLine("Blog Title: ");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Blog Author: ");
+        string author = Console.ReadLine();
+
+        Console.WriteLine("Blog Content: ");
+        string content = Console.ReadLine();
+
+        SqlConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
+        string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        cmd.Parameters.AddWithValue("@BlogTitle", title);
+        cmd.Parameters.AddWithValue("@BlogAuthor", author);
+        cmd.Parameters.AddWithValue("@BlogContent", content);
+
+        int result = cmd.ExecuteNonQuery();
+        connection.Close();
+        Console.WriteLine(result == 1 ? "Saving Successful" : "Saving Failed");
+    }
+
+}
--- DotNetTrainingBatch5.ConsoleApp/Program.cs
+++ DotNetTrainingBatch5.ConsoleApp/Program.cs
@@ -1,65 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 using System.Data.SqlClient;
+using AdoDotNetExample;
 
 Console.WriteLine("Hello, Myanmar Pyi Ka Lu Twy!");
 
-string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
-Console.WriteLine("Connection string", connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
-
-Console.WriteLine("Connection opening");
-connection.Open();
-Console.WriteLine("Connect opened");
-
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_Blog]";
-SqlCommand cmd = new SqlCommand(query, connection);
-// SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-// DataTable dt = new DataTable();
-// adapter.Fill(dt);
-SqlDataReader reader = cmd.ExecuteReader();
-
-while (reader.Read())
-{
-    Console.WriteLine($"{reader["BlogId"]} {reader["BlogTitle"]} {reader["BlogAuthor"]} {reader["BlogContent"]}");
-}
-connection.Close();
-
-Console.WriteLine("Blog Title: ");
-string title = Console.ReadLine();
-
-Console.WriteLine("Blog Author: ");
-string author = Console.ReadLine();
-
-Console.WriteLine("Blog Content: ");
-string content = Console.ReadLine();
-
-connection.Open();
-string queryInsert = @"INSERT INTO [dbo].[Tbl_Blog]
-           ([BlogTitle]
-           ,[BlogAuthor]
-           ,[BlogContent]
-           ,[DeleteFlag])
-     VALUES
-           (@BlogTitle
-           ,@BlogAuthor
-           ,@BlogContent
-           ,0)";
-
-SqlCommand cmd2 = new SqlCommand(queryInsert, connection);
-cmd2.Parameters.AddWithValue("@BlogTitle", title);
-cmd2.Parameters.AddWithValue("@BlogAuthor", author);
-cmd2.Parameters.AddWithValue("@BlogContent", content);
-// SqlDataAdapter adapter = new SqlDataAdapter(cmd2);
-// DataTable dt = new DataTable();
-// adapter.Fill(dt);
-
-int result = cmd2.ExecuteNonQuery();
-connection.Close();
-Console.WriteLine(result == 1 ? "Saving Successful" : "Saving Failed");
+AdoDotNet ado = new AdoDotNet();
+// ado.Create();
+ado.Read();
 Console.ReadKey();
