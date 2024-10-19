3ed8a4a062cf83d0cb898f40371d58a4e0d05d63
Wunna <wunnaaungsayargyi99@gmail.com>
2024-10-19 09:55:43 +0700

    feat: another testing with Dapper

---
+++ DotNetTrainingBatch5.ConsoleApp/DapperConnection.cs
@@ -0,0 +1,120 @@
+﻿using Dapper;
+using DotNetTrainingBatch5.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch5.ConsoleApp
+{
+    public class DapperConnection
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+
+        public void Read()
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM Tbl_Blog WHERE DeleteFlag = 0;";
+                var lst = db.Query<BlogDataModel>(query).ToList();
+                foreach (var item in lst) 
+                {
+                    Console.WriteLine(item.BlogId); 
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                }
+            }       
+                
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                int result = db.Execute(query, new BlogDataModel
+                {
+                    BlogTitle = title,
+                    BlogAuthor = author,
+                    BlogContent = content
+                });
+                Console.WriteLine(result == 1 ? "Insert Successful" : "Insert Failed");
+            }
+        }
+
+        public void Edit(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM Tbl_Blog WHERE DeleteFlag = 0 and BlogId = @BlogId";
+                var item = db.Query<BlogDataModel>(query, new BlogDataModel()
+                {
+                    BlogId = id
+                }).FirstOrDefault();
+
+                if (item is null)
+                {
+                    Console.WriteLine("No data found!");
+                    return;
+                }
+
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            }
+        }
+
+        public void Update(int id, string title, string author, string content)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = $@"UPDATE [dbo].[Tbl_Blog]
+             SET [BlogTitle] = @BlogTitle
+                ,[BlogAuthor] = @BlogAuthor
+                ,[BlogContent] = @BlogContent
+                ,[DeleteFlag] = 0
+                WHERE BlogId = @BlogId";
+
+                var item = db.Execute(query, new BlogDataModel
+                {
+                    BlogId = id,
+                    BlogTitle = title,
+                    BlogAuthor = author,
+                    BlogContent = content
+                });
+                Console.WriteLine(item == 1 ? "Update Successful" : "Update Failed");
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = $@"UPDATE Tbl_Blog SET DeleteFlag = 1 WHERE BlogId = @BlogId";
+
+                var result = db.Execute(query, new BlogDataModel
+                {
+                    BlogId = id
+                });
+                Console.WriteLine(result == 1 ? "Delete Successful" : "Delete Failed");
+            }
+        }
+
+
+    }
+}
--- DotNetTrainingBatch5.ConsoleApp/Program.cs
+++ DotNetTrainingBatch5.ConsoleApp/Program.cs
@@ -14,14 +14,20 @@ AdoDotNet ado = new AdoDotNet();
 // ado.Update();
 // ado.Delete();
 
-DapperExample dapperExample = new DapperExample();
+//DapperExample dapperExample = new DapperExample();
 
 // dapperExample.Create("ကိုယ်ကျင့်တရားကောင်းဖို့", "ဆရာတော်ဘုရား", "ကိုယ်လဲ ပဲ ကြိုးပမ်းအားထုတ်ရမယ်");
 // dapperExample.Create("တရားဓမ္မနဲ့မွေ့လျော်နေ", "ဆရာတော်ဘုရား", "ဆရာတော်ဘုရားက အမှာတော်ကြား");
 
 // dapperExample.Update(3, "တရားဒေသနာ", "ဆရာတော်ဘုရား", "ဆုံးမတော်မူပေထ");
 // dapperExample.Delete(1);
-EFCoreExample efCoreExample = new EFCoreExample();
-efCoreExample.Read();
+//EFCoreExample efCoreExample = new EFCoreExample();
+//efCoreExample.Read();
 
+DapperConnection dapperConnection = new DapperConnection();
+//dapperConnection.Read();
+//dapperConnection.Create("ကိုယ်ကျင့်တရားကောင်းဖို့", "ဆရာတော်ဘုရား", "ကိုယ်လဲ ပဲ ကြိုးပမ်းအားထုတ်ရမယ်");
+//dapperConnection.Edit(2);
+//dapperConnection.Update(1, "Hee Hee", "Lil Z", "ae kg hee hee har har lote");
+dapperConnection.Delete(2);
 Console.ReadKey();
